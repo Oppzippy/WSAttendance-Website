56af563ea344ff7e9941deb5fc28f214b546b7fd
Kyle Scheuing <oppzippy@gmail.com>
2019-05-23 03:01:30 -0400

    Add guilds table

---
+++ WSAttendance/database/migrations/2019_05_23_065816_create_guilds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuildsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guilds', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('owner_id');
+            $table->string('name')->unique();
+            $table->string('visibility')->default('unlisted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guilds');
+    }
+}
